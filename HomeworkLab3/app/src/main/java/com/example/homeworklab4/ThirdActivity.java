7a4715a44cdf4237cf97755de3594e8274603d4e
Vlad <vladaron123@gmail.com>
2019-11-19 18:44:28 +0200

    Homework lab 3

---
+++ HomeworkLab3/app/src/main/java/com/example/homeworklab4/ThirdActivity.java
@@ -0,0 +1,41 @@
+package com.example.homeworklab4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class ThirdActivity extends AppCompatActivity {
+
+    View firstView;
+    View secondView;
+    View thirdView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.puzzle_game);
+
+        initViews();
+
+        firstView.setX(750);
+        firstView.setY(-560);
+
+        secondView.setX(325);
+        secondView.setY(-335);
+
+        thirdView.setX(secondView.getX() + 80);
+        thirdView.setY(secondView.getY() - 150);
+
+    }
+
+
+    void initViews(){
+        firstView = findViewById(R.id.firstView);
+        secondView = findViewById(R.id.secondView);
+        thirdView = findViewById(R.id.thirdView);
+    }
+}
